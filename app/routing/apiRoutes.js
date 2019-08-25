f1fec3326d56f9c5f94b25d542da989ec96aa538
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-08-25 15:30:34 -0400

    Started on my server and friends js files, exported the friend file into the server

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,3 @@
+// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,3 @@
+// A GET Route to /survey which should display the survey page.
+// A default, catch-all route that leads to home.html which displays the home page.
+
